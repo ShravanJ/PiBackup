56652fba5997830602bafae5f85768ebd421ffb7
Shravan Jambukesan <shravan.j97@gmail.com>
2017-03-14 20:18:51 -0500

    Update 1.0.1
    Added status information during backup

--- backup.sh
+++ backup.sh
@@ -1,22 +1,35 @@
-#! /bin/bash
+#!/bin/bash
 
 #Replace 'shravan' with your username
 #Modify so that your desired directories are backed up. Use this as a reference.
-#In this case I backup my /www/ webpages dir and my home directory and copy to a mounted USB drive.
+#In this case I backup my /www/ webpages dir and my home directory and copy to a mounted USB drive (mounted at point /mnt/usb in this case).
+#You can also modify the status information based on what files/directories you are backing up
+
+echo "PiBackup v1.0.1"
+echo "Starting backup..."
 
 cd /home/
-
-sudo tar czf shravan_home.tar.gz shravan 
-
+echo "Creating archive for home directory..."
+sudo tar czf shravan_home.tar.gz shravan
+echo "Created archive for home directory"
 
 cd /var/
-
+echo "Creating archive for web pages..."
 sudo tar czf webpages.tar.gz www
-
+echo "Created archive for web pages"
+echo "Creating archive for build server..."
+sudo tar czf build.tar.gz build
+echo "Created archive for build server"
+echo "Moving archives to home..."
 sudo mv /var/webpages.tar.gz /home/
+sudo mv /var/build.tar.gz /home/
+echo "Moved archives to home"
 
 cd /home/
-
+echo "Moving archives to external storage..."
 sudo mv webpages.tar.gz /mnt/usb
 sudo mv shravan_home.tar.gz /mnt/usb
+sudo mv build.tar.gz /mnt/usb
+echo "Archives moved"
 
+echo "Backup completed"
